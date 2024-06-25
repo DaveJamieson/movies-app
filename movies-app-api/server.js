b8918bbf4ae420ed62cefc662eb793e91d428f21
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2024-06-25 16:43:34 +1200

    updated production build

--- movies-app-api/server.js
+++ movies-app-api/server.js
@@ -17,8 +17,8 @@ app.use(requestLogger);
 
 app.use(
 	cors({
-		origin: "http://localhost:3000",
-		// https://movies-app-j7xn.onrender.com"
+		// frontend url goes here
+		origin: "https://movies-app-j7xn.onrender.com",
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		credentials: true,
 	})
